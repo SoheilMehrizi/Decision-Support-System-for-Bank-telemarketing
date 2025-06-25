3765cf3180587a9610c5fab1efcff1e2a22d7caa
SoheilMehrizi <mehrizisoheil@gmail.com>
2025-06-26 00:28:22 +0330

    Update logistic regression trainer module.

--- src/models/logistic_regression.py
+++ src/models/logistic_regression.py
@@ -5,13 +5,16 @@ from sklearn.model_selection import HalvingRandomSearchCV, StratifiedKFold
 
 from scipy.stats import loguniform
 
-from imblearn.pipeline import Pipeline
+from imblearn.pipeline import Pipeline as ImbPipeline
 from imblearn.over_sampling import RandomOverSampler
 
 from src.models.model_repository import ModelRepository
 from configs.config_repository import ConfigRepository
+from sklearn.preprocessing import FunctionTransformer
 
-def train_logistic_regression(X_train, y_train, cv_splits= 5, random_state=42):
+
+def train_logistic_regression(X_train, y_train, preprocessing_pipeline,
+                               cv_splits= 5, random_state=42):
 
     repo = ConfigRepository(config_path="../configs/models_config.json")
     param_dist = repo.get_config("logistic_regression")
@@ -20,13 +23,17 @@ def train_logistic_regression(X_train, y_train, cv_splits= 5, random_state=42):
     
     lr_model = LogisticRegression(max_iter=1000, random_state=random_state)
     
-    pipeline = Pipeline([
-    ("smote", RandomOverSampler(random_state=42)),
-    ("clf", lr_model)
+    
+    pipeline = ImbPipeline([
+       ('preprocess', FunctionTransformer(preprocessing_pipeline.fit_transform, validate=False)),
+       ("smote", RandomOverSampler(random_state=42)),
+       ("clf", lr_model) 
     ])
 
+
+
     halving_search = HalvingRandomSearchCV(
-        estimator = lr_model,
+        estimator = pipeline,
         param_distributions=param_dist,
         factor = 3,                    
         resource="n_samples",              
@@ -41,12 +48,21 @@ def train_logistic_regression(X_train, y_train, cv_splits= 5, random_state=42):
     halving_search.fit(X_train, y_train)
     
     best_model = halving_search.best_estimator_
+    
     best_params = halving_search.best_params_
+    #TODO: log the reference pipeline.
     
     metrics = {"cv_roc_auc": halving_search.best_score_}
     
+    trained_clf = best_model.named_steps['clf']
+
+    refernce_pipeline = ImbPipeline([
+       ('preprocess', FunctionTransformer(preprocessing_pipeline.fit_transform, validate=False)),
+       ("clf", lr_model) 
+    ])
+
     repo = ModelRepository(experiment_name="Bank_Marketing_Models")
-    run_id = repo.log_model(best_model, model_name="logistic_regression_model", params=best_params, metrics=metrics)
-    registered_model = repo.register_model(run_id, model_name="logistic_regression_model", registered_name="BankMarketing_LogReg")
+    run_id = repo.log_model(refernce_pipeline, model_name="logistic_regression_model", params=best_params, metrics=metrics)
+    registered_model = repo.register_model(run_id, model_name="logistic_regression_model", registered_name="LogReg")
     
     return best_model, best_params, metrics
