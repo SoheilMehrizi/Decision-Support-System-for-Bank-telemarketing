5a001d7978147604440800555abcd2b574aef72b
SoheilMehrizi <mehrizisoheil@gmail.com>
2025-06-27 13:17:43 +0330

    Update neural network.py due to preprocessing pipeline update.

--- src/models/neural_network.py
+++ src/models/neural_network.py
@@ -3,6 +3,7 @@ import pandas as pd
 import tempfile
 
 from sklearn.model_selection import train_test_split
+from imblearn.pipeline import Pipeline as ImbPipeline
 
 import tensorflow as tf
 from keras import layers, callbacks, optimizers, metrics
@@ -12,8 +13,10 @@ import keras_tuner as kt
 
 from configs.config_repository import ConfigRepository
 from src.models.model_repository import ModelRepository
+from sklearn.preprocessing import FunctionTransformer
 
-def build_model(hp, input_dim: int, mlp_config: dict) -> tf.keras.Model:
+
+def build_model(hp,input_dim: int, mlp_config: dict) -> tf.keras.Model:
     """
     Build a Keras Sequential model using hyperparameters from configuration.
     """
@@ -41,8 +44,8 @@ def build_model(hp, input_dim: int, mlp_config: dict) -> tf.keras.Model:
     return model
 
 
-def train_mlp(X_train, y_train,
-              test_size:int = 0.2,
+def train_mlp(X_train, y_train, preprocessing_pipeline,
+              dev_size:int = 0.2,
               random_state: int = 42) -> tuple:
     """
     Train an MLP with hyperparameter tuning using configs and log the final model.
@@ -51,26 +54,36 @@ def train_mlp(X_train, y_train,
     repo_cfg = ConfigRepository(config_path="../configs/models_config.json")
     mlp_config = repo_cfg.get_config('mlp')
 
-    X = X_train.values
-    y = y_train.values
-    input_dim = X.shape[1]
+    y_train = y_train.to_frame()
+    y_train['y'] = y_train['y'].map({'yes':1, 'no': 0})
+
+    X = X_train
+    y = y_train['y']
 
     X_train, X_val, y_train, y_val = train_test_split(
         X, y,
-        test_size=test_size,
+        test_size=dev_size,
         random_state=random_state,
         shuffle=True,
         stratify=y
     )
+    preprocessing_pipeline.fit(X_train, y_train)
+    X_tr_prp = preprocessing_pipeline.transform(X_train)
+    X_val_prp = preprocessing_pipeline.transform(X_val)
+
+    X_full_prp = preprocessing_pipeline.transform(X)
 
     ros = RandomOverSampler(random_state=random_state)
-    X_res, y_res = ros.fit_resample(X_train, y_train)
+    X_tr_rsmpld, y_tr_rsmpld = ros.fit_resample(X_tr_prp, y_train)
+
+    X_full_rsmpld, y_full_rsmpld = ros.fit_resample(X_full_prp, y)
 
     stop = callbacks.EarlyStopping(
         monitor='val_auc', patience=mlp_config.get('patience', 10),
         mode='max', restore_best_weights=True
     )
 
+    input_dim = X_tr_rsmpld.shape[1]
     tuner = kt.Hyperband(
         hypermodel=lambda hp: build_model(hp, input_dim, mlp_config),
         objective=kt.Objective("val_auc", direction="max"),
@@ -83,15 +96,15 @@ def train_mlp(X_train, y_train,
 
 
     tuner.search(
-        X_res, y_res,
+        X_tr_rsmpld, y_tr_rsmpld,
         epochs= mlp_config.get('search_epochs', 50),
-        validation_data=(X_val, y_val),
+        validation_data=(X_val_prp, y_val),
         callbacks=[stop],
         verbose=3
     )
 
     best_model = tuner.get_best_models(num_models=1)[0]
-    y_val_pred = best_model.predict(X_val).ravel()
+    y_val_pred = best_model.predict(X_val_prp).ravel()
 
     auc = roc_auc_score(y_val, y_val_pred)
 
@@ -100,18 +113,27 @@ def train_mlp(X_train, y_train,
     best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
 
     print(f"\nBest Candidate's AUC: {auc:.4f}")
-
+    print("Best hyperparameters:", best_hps.values)
+    best_model.summary()
+    
+    print(5*"*","Retrain the best Estimator on the full training set",5*"*")
+    # FINAL TRAINING
     final_model = build_model(best_hps, input_dim, mlp_config)
     final_model.fit(
-        X, y,
+        X_full_rsmpld, y_full_rsmpld,
         epochs=mlp_config.get('final_epochs', 50),
         batch_size=mlp_config.get('batch_size', 32),
         verbose=2
     )
 
+    refernce_pipeline = ImbPipeline([
+       ('preprocess', FunctionTransformer(preprocessing_pipeline.transform, validate=False)),
+       ("clf", final_model)
+    ])
+
     repo_mdl = ModelRepository(experiment_name=mlp_config.get('experiment_name', 'MLP_Models'))
     run_id = repo_mdl.log_model(
-        final_model,
+        refernce_pipeline,
         model_name=mlp_config.get('model_name', 'MLP'),
         params=best_hps.values,
         metrics={'roc_auc': best_auc}
@@ -122,4 +144,4 @@ def train_mlp(X_train, y_train,
         registered_name=mlp_config.get('registered_name', 'MLP')
     )
 
-    return final_model, best_hps, auc
+    return refernce_pipeline, best_hps, auc
