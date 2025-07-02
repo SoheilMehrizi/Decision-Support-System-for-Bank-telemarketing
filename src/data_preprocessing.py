57eb592943817efc0f3f9e01fddb409b732c40d2
SoheilMehrizi <mehrizisoheil@gmail.com>
2025-07-02 13:44:35 +0330

    refactor preprocessing transformer pipeline.

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -5,7 +5,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
-from .data_cleaning import DataCleaningTransformer, SeasonalDecompositionOutlierRemover
+from .data_cleaning import DataCleaningTransformer, BoxplotOutlierRemover
 from .feature_engineering import DynamicNumToCatTransformer
 import warnings
 
@@ -27,16 +27,17 @@ def create_preprocessor(num_features:list, cat_features:list):
     ])
     return preprocessor
 
-
-def preprocess_pipeline(
-        num_columns:list,
-        cat_columns:list,
+def cleaning_pipeline_step(
+        df,
         missing_method = 'drop',
         fill_value=None,
         fill_method=None,
         missing_threshold=0.5,
         outlier_removal=False,
         ):
+    
+    numeric_features = df.select_dtypes(include=['number']).columns.tolist()
+
     steps = [
         ('cleaning', DataCleaningTransformer(
             missing_method=missing_method,
@@ -47,13 +48,11 @@ def preprocess_pipeline(
     ]
 
     if outlier_removal:
-        steps.append(('outlier_removal', SeasonalDecompositionOutlierRemover(
-            numeric_columns=num_columns, cat_columns=cat_columns, period=12)))
+        steps.append(('outlier_removal', 
+                      BoxplotOutlierRemover(numeric_features=numeric_features)))
 
-    column_transformer = create_preprocessor(num_features=num_columns, cat_features=cat_columns)
+    full_cleaning_pipeline = Pipeline(steps)
 
-    steps.append(('column_transformer', column_transformer))
+    df_cleaned = full_cleaning_pipeline.fit_transform(df)
 
-    full_preprocessing_pipeline = Pipeline(steps)
-
-    return full_preprocessing_pipeline
+    return df_cleaned
